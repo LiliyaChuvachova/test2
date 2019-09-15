79bac2b97a33e50a3fc933884f8d4947b54812e2
LiliyaChuvachova <babak.liliya@mail.ru>
2019-09-16 00:35:39 +0300

    add function initSlider

--- carusel.js
+++ carusel.js
@@ -1,46 +1,47 @@
+/*
 $(document).ready(function() {
 $(".gallery2").jCarouselLite({
-btnNext: ".next", // класс кнопки перехода вперед, null для отключения
-btnPrev: ".prev", // класс кнопки перехода назад, null для отключения
-mouseWheel: false, //если false, то галерея не будет прокручиваться колесом мыши
-auto: null, //шаг автопрокрутки в миллисекундах, null для отключения 
-speed: null, //скорость прокрутки в миллисекундах
-vertical: false, //если true, то галерея будет располагаться вертикально
-circular: true, //true - прокрутка по кругу, false - только до последнего изображения
-visible: 4, //количество видимых картинок
-start: 0, //с какого элемента начинается галерея (первый элемент имеет значение 0)
-scroll: 1 //количество элементов, которые одновременно прокручиваются при щелчке по кнопке
+btnNext: ".next", 
+btnPrev: ".prev", 
+mouseWheel: false, 
+auto: null, 
+speed: null, 
+vertical: false, 
+circular: true, 
+visible: 4, 
+start: 0, 
+scroll: 1 
 });
 });
 
 
 $(document).ready(function() {
 $(".gallery1").jCarouselLite({
-btnNext: ".next1", // класс кнопки перехода вперед, null для отключения
-btnPrev: ".prev1", // класс кнопки перехода назад, null для отключения
-mouseWheel: false, //если false, то галерея не будет прокручиваться колесом мыши
-auto: null, //шаг автопрокрутки в миллисекундах, null для отключения 
-speed: null, //скорость прокрутки в миллисекундах
-vertical: false, //если true, то галерея будет располагаться вертикально
-circular: true, //true - прокрутка по кругу, false - только до последнего изображения
-visible: 4, //количество видимых картинок
-start: 0, //с какого элемента начинается галерея (первый элемент имеет значение 0)
-scroll: 1 //количество элементов, которые одновременно прокручиваются при щелчке по кнопке
+btnNext: ".next1", 
+btnPrev: ".prev1",
+mouseWheel: false, 
+auto: null, 
+speed: null, 
+vertical: false, 
+circular: true, 
+visible: 4, 
+start: 0, 
+scroll: 1
 });
 });
 
 $(document).ready(function() {
 $(".gallery3").jCarouselLite({
-btnNext: ".next3", // класс кнопки перехода вперед, null для отключения
-btnPrev: ".prev3", // класс кнопки перехода назад, null для отключения
-mouseWheel: false, //если false, то галерея не будет прокручиваться колесом мыши
-auto: null, //шаг автопрокрутки в миллисекундах, null для отключения 
-speed: null, //скорость прокрутки в миллисекундах
-vertical: false, //если true, то галерея будет располагаться вертикально
-circular: true, //true - прокрутка по кругу, false - только до последнего изображения
-visible: 4, //количество видимых картинок
-start: 0, //с какого элемента начинается галерея (первый элемент имеет значение 0)
-scroll: 1 //количество элементов, которые одновременно прокручиваются при щелчке по кнопке
+btnNext: ".next3", 
+btnPrev: ".prev3", 
+mouseWheel: false, 
+auto: null,  
+speed: null, 
+vertical: false, 
+circular: true,
+visible: 4, 
+start: 0, 
+scroll: 1 
 });
 });
 
@@ -58,3 +59,30 @@ start: 0, //с какого элемента начинается галерея
 scroll: 1 //количество элементов, которые одновременно прокручиваются при щелчке по кнопке
 });
 });
+*/
+
+$(document).ready(function() { 
+function initSlider(galleryClass, nextBtnClass, previousBtnClass) {
+$(galleryClass).jCarouselLite({
+btnNext: nextBtnClass, // класс кнопки перехода вперед, null для отключения
+btnPrev: previousBtnClass, // класс кнопки перехода назад, null для отключения
+mouseWheel: false, //если false, то галерея не будет прокручиваться колесом мыши
+auto: null, //шаг автопрокрутки в миллисекундах, null для отключения 
+speed: null, //скорость прокрутки в миллисекундах
+vertical: false, //если true, то галерея будет располагаться вертикально
+circular: true, //true - прокрутка по кругу, false - только до последнего изображения
+visible: 4, //количество видимых картинок
+start: 0, //с какого элемента начинается галерея (первый элемент имеет значение 0)
+scroll: 1 //количество элементов, которые одновременно прокручиваются при щелчке по кнопке
+});	
+}
+initSlider($(".gallery1"), $(".next1"), $(".prev1"));
+initSlider($(".gallery2"), $(".next"), $(".prev"));
+initSlider($(".gallery3"), $(".next3"), $(".prev3"));
+initSlider($(".gallery4"), $(".next4"), $(".prev4"));
+});
+
+
+
+
+
